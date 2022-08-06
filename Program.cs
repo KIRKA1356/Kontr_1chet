688c49756be35f7bb3dcb12b952db2af2af9889f
KIRKA1356 <kgritsun@mail.ru>
2022-08-06 14:46:15 +0300

    Метод генерирующий массив строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.WriteLine("Введите кол-во слов, которое будете вводить");
 int size = Convert.ToInt32(Console.ReadLine());
+
+string[] Array(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
